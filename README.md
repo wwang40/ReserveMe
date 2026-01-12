5611208e541c742353f1f04e0686a086f2b5f9a7
William Wang <ww548@cornell.edu>
2026-01-11 21:04:07 -0500

    Spring Init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ReserveMe
+
 A Springboot application that allows users to reserve each other's time. 
---
+++ src/main/java/org/reserv/reserveme/ReserveMeApplication.java
@@ -0,0 +1,13 @@
+package org.reserv.reserveme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveMeApplication.class, args);
+    }
+
+}
